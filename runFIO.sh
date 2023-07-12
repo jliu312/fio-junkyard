bd590fea5a15f3c973a5499f7dafc9dfbfb87369
Jacob Liu <liu.jacob2004@gmail.com>
2023-07-12 19:55:42 +0000

    wrote script for FIO rand/seq read/write

--- runFIO.sh
+++ runFIO.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 echo running random read
-fio fio-rand-read.fio
+fio fio-rand-read.fio>./output/randread.txt
 echo running random write
-fio fio-rand-write.fio
+fio fio-rand-write.fio>./output/randwrite.txt
 echo running sequential read
-fio fio-seq-read.fio
+fio fio-seq-read.fio>./output/seqread.txt
 echo running sequential write
-fio fio-seq-write.fio
+fio fio-seq-write.fio>./output/seqwrite.txt
 
 
